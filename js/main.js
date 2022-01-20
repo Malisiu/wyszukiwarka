2695324ac0dabbdb3ea0af1257a7ae4beaa98f56
Michał Malicki <michalmalicki@op.pl>
2022-01-20 15:35:16 +0100

    finished search app

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-const  ul = document.querySelector('ul')
-const li = document.querySelectorAll('li')
-const input = document.querySelector('.search')
+const li = document.querySelectorAll('li');
+const input = document.querySelector('.search');
 
-input.addEventListener('keyup' ,function(){
-    for(let i = 0; i < li.length; i++){
-        li[i].setAttribute('style','display: block')
-        if(li[i].textContent.toUpperCase().includes(input.value.toUpperCase()) === false){
-            li[i].setAttribute('style','display:none')
+const searchEngine = e => {
+    
+    const text = e.target.value;
+    console.log(text);
+    
+    li.forEach(el => {
+        if(el.textContent.toUpperCase().indexOf(text.toUpperCase()) === -1){
+            el.style.display= 'none'
+        }else {
+            el.style.display ='block'
         }
-    }
-})
+    })
 
+}
+input.addEventListener('keyup',searchEngine)
